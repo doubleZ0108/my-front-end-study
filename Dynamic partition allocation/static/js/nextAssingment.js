6b8c26ba63478869bcb3ba59a2ac94c028b823a5
double Z <39005525+doubleZ0108@users.noreply.github.com>
2019-06-01 09:53:21 +0800

    add 最佳适配逻辑  --> update 逻辑组织结构  --> add 注释

--- Dynamic partition allocation/static/js/nextAssingment.js
+++ Dynamic partition allocation/static/js/nextAssingment.js
@@ -13,6 +13,23 @@ function Proj(name, data) {
         return this.data
     }
 }
+
+/**
+ * 标记类
+ * @param {起始地址} start 
+ * @param {长度} last 
+ */
+function Mark(start, last) {
+    this.start = start;
+    this.last = last;
+    this.getstart = function () {
+        return this.start;
+    }
+    this.getlast = function () {
+        return this.last;
+    }
+}
+
 /**
  * 作业列表(后期可考虑动态扩充)
  */
@@ -30,7 +47,37 @@ const projList = [
     new Proj("作业6", -60)
 ]
 
-var now = 0;
+var now = 0;    //当前执行到的指令位置
+
+occupyMem = [] //被占用标记表
+useableMem = [] //空闲空间标记表
+
+const ADDSUCCESS = 0;       //添加作业成功
+const ADDFAILED = 1;        //添加作业失败
+const REMOVESUCCESS = 2;    //释放作业成功
+
+/**Mark对象排序 => 依据start的大小 */
+var compareStart = function (x, y) {
+    if (x.getstart() < y.getstart()) {
+        return -1;
+    } else if (x.getstart() > y.getstart()) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+/**Mark对象排序 => 依据last的大小 */
+var compareLast = function (x, y) {
+    if (x.getlast() < y.getlast()) {
+        return -1;
+    } else if (x.getlast() > y.getlast()) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+
 
 
 /**
@@ -45,19 +92,15 @@ function nextAssignment() {
     var Mem = document.getElementById('memory'); //获取内存实体
     var Board = document.getElementById('board'); //获取告示板实体
 
+    if (occupyMem == false) { //内存没有任何作业
+        useableMem.push(new Mark(0, memSize));
+    }
+
     if (now < projList.length) {
-        if (algorithm == "首次适应算法") {
-            if (firstAdapt(memSize, Mem, Board)) { //可以装入作业
-                now++;
-            } else {
-                ;
-            }
-        } else if (algorithm == "最佳适应算法") {
-            if(bestAdapt(memSize, Mem, Board)){
-                now++;
-            }else{
-                ;
-            }
+        if(Adapt(memSize,Mem,Board,algorithm)){//可以装入作业
+            now++;
+        }else{
+            ;
         }
     } else {
         var mess = document.createElement("mess");
@@ -73,55 +116,28 @@ function nextAssignment() {
     Board.scrollTop = Board.scrollHeight;
 }
 
-/**
- * 使用标记
- * @param {起始地址} start 
- * @param {长度} last 
- */
-function Mark(start, last) {
-    this.start = start;
-    this.last = last;
-    this.getstart = function () {
-        return this.start;
-    }
-    this.getlast = function () {
-        return this.last;
-    }
-}
 
-occupyMem = [] //被占用标记表
-useableMem = [] //空闲空间标记表
-
-const ADDSUCCESS = 0;
-const ADDFAILED = 1;
-const REMOVESUCCESS = 2;
-
-/**Mark对象排序依据start的大小 */
-var compare = function (x, y) {
-    if (x.getstart() < y.getstart()) {
-        return -1;
-    } else if (x.getstart() > y.getstart()) {
-        return 1;
-    } else {
-        return 0;
-    }
-}
-
-function firstAdapt(memSize, Mem, Board) {
-    if (occupyMem == false) { //内存没有任何作业
-        useableMem.push(new Mark(0, memSize));
-    }
-
+/**适配算法 */
+function Adapt(memSize, Mem, Board, algorithm) {
     var name = projList[now].getname();
     var data = projList[now].getdata();
 
     if (data > 0) { //申请空间
+        if(algorithm == "首次适应算法"){
+            useableMem.sort(compareStart)
+        }else if(algorithm == "最佳适应算法"){
+            useableMem.sort(compareLast);
+        }
+        console.log("after algorithm filter...", useableMem)
+
         for (var i = 0; i < useableMem.length; ++i) {
             if (useableMem[i].getlast() > data) { //第一个能放下的位置
                 start = useableMem[i].getstart();
                 last = useableMem[i].getlast();
 
                 occupyMem.push(new Mark(start, data));
+                occupyMem.sort(compareStart)
+
                 useableMem[i].start += data;
                 useableMem[i].last -= data;
 
@@ -158,10 +174,6 @@ function firstAdapt(memSize, Mem, Board) {
 
     addMess(Board, name, data, -1, ADDFAILED);
     return false;
-}
-
-function bestAdapt(memSize, Mem, Board) {
-
 }
 
 /**
@@ -214,15 +226,15 @@ function addMess(Board, name, data, start, flag) {
 
 /**重新整理useableMem */
 function update() {
-    useableMem.sort(compare)
+    useableMem.sort(compareStart)
     console.log("before update", useableMem)
 
     var i = 0;
     while (i < useableMem.length) {
-        while (i + 1 < useableMem.length &&
+        while (i + 1 < useableMem.length &&             //当前空闲块和后面的空闲块可以合并,持续循环合并
             (useableMem[i].getstart() + useableMem[i].getlast() == useableMem[i + 1].getstart())) {
             useableMem[i].last += useableMem[i + 1].getlast();
-            useableMem.splice(i + 1, 1);
+            useableMem.splice(i + 1, 1);    //合并后删除后面的空闲块
         }
         ++i;
     }
