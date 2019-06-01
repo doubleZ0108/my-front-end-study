d17abde87dc71b5635532e401d1ee3ef8c2190ba
double Z <39005525+doubleZ0108@users.noreply.github.com>
2019-06-01 09:47:59 +0800

    update 清空内存逻辑

--- Dynamic partition allocation/static/js/clear.js
+++ Dynamic partition allocation/static/js/clear.js
@@ -4,4 +4,14 @@ function clearbtnClick(){
     while(Mem.hasChildNodes()){
         Mem.removeChild(Mem.firstChild)
     }
+
+    var Board = document.getElementById('board');
+    while(Board.hasChildNodes()){
+        Board.removeChild(Board.firstChild)
+    }
+
+    now = 0;        //重置作业列表(从头开始作业调度)
+    occupyMem.length = 0;   //清空占用列表
+    useableMem.length = 0;  //清空可用列表
+    $("#memory-size").removeClass("disable")    //重置滑动条为可用
 }
