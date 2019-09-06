c3c55c59622a3a58047952805e9139b17632b457
double Z <39005525+doubleZ0108@users.noreply.github.com>
2019-09-06 22:33:47 +0800

    Update DOM事件

--- 100-study/JavaScript/DOM/Event/common.js
+++ 100-study/JavaScript/DOM/Event/common.js
@@ -1,3 +1,4 @@
+/*页面加载/离开后*/
 window.onload = function () {
 
 };
@@ -6,6 +7,8 @@ window.onunload = function () {
     //alert('真的要退出么');  不会阻止用户退出
 };
 
+
+/*鼠标事件*/
 function changeText(elem){
     elem.innerHTML = '被点击之后的文本';
 }
@@ -19,6 +22,18 @@ function mymouseOut(elem) {
     elem.innerText = '把鼠标移上来试试';
 }
 
+
+/*输入字段被改变时*/
 function UpperCase(elem) {
     elem.value = elem.value.toUpperCase();
+}
+
+
+/*获得到焦点/失去焦点*/
+function focusChange(elem, focus_flag) {
+    if(focus_flag){
+        elem.style.backgroundColor = 'lightpink';
+    }else{
+        elem.style.backgroundColor = 'white';
+    }
 }
